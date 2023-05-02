795c9a57bd08d11b00e47f951a0dfdbed7155894
gbesttech <122006219+gbesttech@users.noreply.github.com>
2023-05-02 08:37:25 -0700

    commit

--- main.js
+++ main.js
@@ -26,23 +26,6 @@ faqs.forEach(faq => {
 })
 
 
-// swiper mySwiper
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    // whhen window width is > 600px
-    breakpoints: {
-        600: {
-            slidesPerView: 2,
-        }
-    }
-  });
-
 
   //show/hide nav menu
 
